f4a0735483d7e55ec97920c6147461093bd43164
Caleb Wright <calebwright156@gmail.com>
2022-11-02 13:05:59 +1300

    started client and project queries

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const colors = require('colors')
 const { grapgqlHTTP, graphqlHTTP } = require('express-graphql')
+const cors = require('cors')
 const schema = require('./schema/schema')
 const connectDB = require('./config/db,')
 const port = process.env.PORT || 5000
@@ -9,6 +10,8 @@ const port = process.env.PORT || 5000
 const app = express()
 
 connectDB()
+
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
     schema,
